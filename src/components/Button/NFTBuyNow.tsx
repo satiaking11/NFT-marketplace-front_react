2011158bb2021ff2d399e78df32f885bab9deb8b
satiaking11 <Santiatop01@gmail.com>
2022-03-11 16:35:20 +0300

    update NFT order page, update positions of NFT information and order button with responsive

---
+++ src/components/Button/NFTBuyNow.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+import React from 'react';
+import Button from './Button';
+
+function NFTBuyNow() {
+  return (
+    <div className="refresh flex flex-col md:flex-row md:items-center justify-between w-full rounded-[40px] py-2 md:pl-[40px] px-2 bg-[#f0f0f0]">
+      <div className="px-10 mb-3 md:mb-0 md:px-0">
+        <p className="text-[16px] text-gray-400 dark:text-light-gray">Price</p>
+        <p className="text-[24px] text-red-400 dark:text-red-800">
+          <b>289</b>
+          {' '}
+          ADA
+          <span className="ml-3 text-[16px] text-gray-400 dark:text-light-gray">($300.34)</span>
+        </p>
+      </div>
+      <Button className="py-2 px-12 text-[16px] h-[64px] md:h-full" color="primary">
+        Buy Now
+      </Button>
+    </div>
+  );
+}
+
+export default NFTBuyNow;
--- src/pages/NFT/index.tsx
+++ src/pages/NFT/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NFTBuyNow from '../../components/Button/NFTBuyNow';
 import useScrollToTop from '../../hooks/useScrollToTop';
 
 function NFT() {
@@ -13,32 +14,37 @@ function NFT() {
             <div className="w-full md:w-[594px]">
               <img src="images/nft/1.png" alt="cover" />
             </div>
-            <div className="w-full md:w-[594px]">
-              <p className="text-[32px] text-black-400 dark:text-light-gray mb-10">NFT name could be so long and a litle bit longer</p>
-              <div className="flex justify-between mb-4">
-                <p className="text-gray-400 dark:text-light-gray">Created By:</p>
-                {' '}
-                <p className="text-link">Kimberly Mastrangelo</p>
+            <div className="w-full md:w-[594px] flex flex-col">
+              <p className="text-[32px] order-1 font-semibold text-black-400 dark:text-light-gray mb-4 md:mb-10">NFT name could be so long and a litle bit longer</p>
+              <div className="order-3 md:order-2 flex flex-col w-full">
+                <div className="flex justify-between mb-4">
+                  <p className="text-gray-400 dark:text-light-gray">Created By:</p>
+                  {' '}
+                  <p className="text-link">Kimberly Mastrangelo</p>
+                </div>
+                <div className="flex justify-between mb-4">
+                  <p className="text-gray-400 dark:text-light-gray">Collection:</p>
+                  {' '}
+                  <p className="max-w-[290px] text-right">Collection name could be so long and a little bit longer</p>
+                </div>
+                <div className="flex justify-between mb-4">
+                  <p className="text-gray-400 dark:text-light-gray">Released:</p>
+                  {' '}
+                  <p>Feb 02, 2021</p>
+                </div>
+                <div className="flex justify-between mb-4">
+                  <p className="text-gray-400 dark:text-light-gray">№ of copies:</p>
+                  {' '}
+                  <p>23</p>
+                </div>
+                <div className="flex justify-between mb-4">
+                  <p className="text-gray-400 dark:text-light-gray">Status:</p>
+                  {' '}
+                  <p className="text-[#00AA0D]">On sale</p>
+                </div>
               </div>
-              <div className="flex justify-between mb-4">
-                <p className="text-gray-400 dark:text-light-gray">Collection:</p>
-                {' '}
-                <p>Collection name could be so long and a little bit longer</p>
-              </div>
-              <div className="flex justify-between mb-4">
-                <p className="text-gray-400 dark:text-light-gray">Released:</p>
-                {' '}
-                <p>Feb 02, 2021</p>
-              </div>
-              <div className="flex justify-between mb-4">
-                <p className="text-gray-400 dark:text-light-gray">№ of copies:</p>
-                {' '}
-                <p>23</p>
-              </div>
-              <div className="flex justify-between mb-4">
-                <p className="text-gray-400 dark:text-light-gray">Status:</p>
-                {' '}
-                <p className="text-[#00AA0D]">On sale</p>
+              <div className="order-2 md:order-3 mb-8 md:mb-0 flex mt-auto">
+                <NFTBuyNow />
               </div>
             </div>
           </div>
